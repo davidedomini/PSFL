0889d787a4dabbfec7e9b7ca951ddb727bf6e4c6
davidedomini <davide.domini@unibo.it>
2025-05-14 17:24:57 +0200

    style: rename psfl function to match python conventions

--- src/PSFLClient.py
+++ src/PSFLClient.py
@@ -1,18 +1,24 @@
-from phyelds.calculus import aggregate
+from phyelds.libraries.device import local_id
+from phyelds.calculus import aggregate, neighbors
 from phyelds.libraries.collect import count_nodes
 from phyelds.libraries.leader_election import elect_leader
 from phyelds.libraries.distances import neighbors_distances
 from phyelds.libraries.spreading import distance_to, broadcast
 
 @aggregate
-def PSFLClient():
+def psfl_client():
     """
     Example to use the phyelds library to create a simple simulation
     :return:
     """
+    loss_based_distance()
     distances = neighbors_distances()
     leader = elect_leader(4, distances)
     potential = distance_to(leader, distances)
     nodes = count_nodes(potential)
     area_value = broadcast(leader, nodes, distances)
     return area_value
+
+def loss_based_distance():
+    mid = local_id()
+    models = neighbors(mid)
--- src/simulation.py
+++ src/simulation.py
@@ -1,5 +1,5 @@
 import random
-from PSFLClient import PSFLClient
+from PSFLClient import psfl_client
 from phyelds.simulator import Simulator
 from phyelds.simulator.render import render_sync
 from phyelds.simulator.deployments import deformed_lattice
@@ -14,7 +14,7 @@ simulator.environment.set_neighborhood_function(radius_neighborhood(1.15))
 deformed_lattice(simulator, 10, 10, 1, 0.01)
 # schedule the main function
 for node in simulator.environment.nodes.values():
-    simulator.schedule_event(0.0, aggregate_program_runner, simulator, 0.1, node, PSFLClient)
+    simulator.schedule_event(0.0, aggregate_program_runner, simulator, 0.1, node, psfl_client)
 # render
 simulator.schedule_event(1.0, render_sync, simulator, "result")
 simulator.run(100)
