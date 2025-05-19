939dd16689692ae53a02c39348351d9443b3aaff
Davide Domini <davide.domini@unibo.it>
2025-05-19 16:06:54 +0200

    feat: add data export

--- src/simulation.py
+++ src/simulation.py
@@ -5,6 +5,7 @@ from phyelds.simulator.render import render_sync
 from phyelds.simulator.deployments import deformed_lattice
 from phyelds.simulator.runner import aggregate_program_runner
 from phyelds.simulator.neighborhood import radius_neighborhood
+from phyelds.simulator.exporter import csv_exporter, ExporterConfig
 
 random.seed(42)
 
@@ -16,5 +17,7 @@ deformed_lattice(simulator, 10, 10, 1, 0.01)
 for node in simulator.environment.nodes.values():
     simulator.schedule_event(0.0, aggregate_program_runner, simulator, 0.1, node, psfl_client)
 # render
-simulator.schedule_event(1.0, render_sync, simulator, "result")
+simulator.schedule_event(0.95, render_sync, simulator, "result")
+config = ExporterConfig('data/', 'experiment', ['TrainLoss', 'ValidationLoss', 'ValidationAccuracy'], ['mean', 'std', 'min', 'max'], 3)
+simulator.schedule_event(1.0, csv_exporter, simulator, 1.0, config)
 simulator.run(100)
