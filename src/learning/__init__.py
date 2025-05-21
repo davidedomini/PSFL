16c876aa97f3e5b1e11a66f835438e0c12e91e4b
Davide Domini <davide.domini@unibo.it>
2025-05-21 10:18:37 +0200

    feat: implement models averaging

--- src/learning/__init__.py
+++ src/learning/__init__.py
@@ -1,8 +1,21 @@
+import copy
+import torch
+
 def local_training(model):
     pass
 
-def model_evaluation(model):
+
+def model_evaluation(model_params):
     pass
 
-def average_weights(model):
-    pass
+
+def average_weights(models_params, weights):
+    w_avg = copy.deepcopy(models_params[0])
+    for key in w_avg.keys():
+        w_avg[key] = torch.mul(w_avg[key], 0.0)
+    sum_weights = sum(weights)
+    for key in w_avg.keys():
+        for i in range(0, len(models_params)):
+            w_avg[key] += models_params[i][key] * weights[i]
+        w_avg[key] = torch.div(w_avg[key], sum_weights)
+    return w_avg
