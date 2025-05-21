aee746f4bf5080a1e0ba025d2ad2271cb690bf86
Davide Domini <davide.domini@unibo.it>
2025-05-21 10:20:38 +0200

    feat: implement local training

--- src/learning/__init__.py
+++ src/learning/__init__.py
@@ -1,8 +1,27 @@
 import copy
 import torch
+from torch import nn
+from torch.utils.data import DataLoader
 
-def local_training(model):
-    pass
+def local_training(model, epochs, data, batch_size):
+    # torch.manual_seed(seed)
+    criterion = nn.NLLLoss()
+    model.train()
+    epoch_loss = []
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
+    data_loader = DataLoader(data, batch_size=batch_size, shuffle=True)
+    for _ in range(epochs):
+        batch_loss = []
+        for batch_index, (images, labels) in enumerate(data_loader):
+            model.zero_grad()
+            log_probs = model(images)
+            loss = criterion(log_probs, labels)
+            loss.backward()
+            optimizer.step()
+            batch_loss.append(loss.item())
+        mean_epoch_loss = sum(batch_loss) / len(batch_loss)
+        epoch_loss.append(mean_epoch_loss)
+    return model.state_dict(), sum(epoch_loss) / len(epoch_loss)
 
 
 def model_evaluation(model_params):
